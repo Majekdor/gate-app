9154ef26ebac13bfa0dbf9424f580b75da5b81e9
Kevin Barnes <kevin@donniebarnes.com>
2022-12-04 17:36:15 -0500

    Initial Commit

---
+++ GateApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GateApp
+//
+//  Created by Kevin Barnes on 12/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GateApp/GateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GateAppApp.swift
+//  GateApp
+//
+//  Created by Kevin Barnes on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
